d4c1893dfd9d24ff17d5a04055406e714b49e2c8
parissa <parissa.eggleston@gmail.com>
2017-04-02 20:39:31 -0400

    change to the karma

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -9,13 +9,13 @@ module UserHelper
   
   def get_karma(score)
     karma_message = ""
-    if score > 0 && score < 5
+    if score >= 0 && score < 5
       karma_message =  'Oh your karma is low, hiring Parissa will help'
-    elsif score > 5 && score < 8 
+    elsif score >= 5 && score < 8 
       karma_message =  'Your karma is getting there, I suggest hiring Parissa to help it '
-    elsif score > 8 && score < 10 
+    elsif score >= 8 && score < 10 
       karma_message =  'Great Karma! You can do even better with Parissa working with you'
-    elsif score > 10
+    elsif score >= 10
       karma_message =  'Namaste! You are the Karma master and worthy to have Parissa work with you'
     else
       karma_message = 'You are beyond hope!'
